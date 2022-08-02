f38d78d38d441e4c3c1f0f7b8515c13f8d7fc0f2
x1unix <denis0051@gmail.com>
2022-08-02 03:40:41 +0200

    vsockets: check sockAddrVm alignment in tests

--- pkg/vmci/vmci_windows_test.go
+++ pkg/vmci/vmci_windows_test.go
@@ -3,12 +3,19 @@ package vmci
 import (
 	"os"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestMain(m *testing.M) {
 	os.Exit(m.Run())
+}
+
+func TestVMSockAddrSize(t *testing.T) {
+	sockAddr := sockAddrVM{}
+	require.Equal(t, len(sockAddr.zero), 4)
+	require.Equal(t, unsafe.Sizeof(sockAddr), uintptr(16))
 }
 
 func TestVersion(t *testing.T) {
